e770dbecfc97a0fc1e877fae405291f1ac0afc8d
Fujimoto Seiji <fujimoto@ceptord.net>
2014-12-25 21:45:10 +0900

    Implement the pseudo POP3 server.

--- test/mockserver.py
+++ test/mockserver.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import socket
+import os
+import time
 
 class MockServer:
     """A base class for server stub"""
@@ -47,3 +49,58 @@ class MockServer:
                 break
         self._recvlog.append(line)
         return line
+
+class POP3Server(MockServer):
+    """A POP3 server stub"""
+    def greeting(self):
+        pid, epoch = os.getpid(), int(time.time())
+        host = self.get_conninfo()[0]
+        msg_id = '<{}.{}@{}>'.format(pid, epoch, host).encode()
+        self.sendline(b'+OK Greetings, human! ' + msg_id)
+
+    # Stub methods
+    def do_stat(self):
+        self.sendline(b'+OK 2 320')
+
+    def do_retr(self):
+        self.sendline('+OK')
+        self.sendline('From: from@example.com')
+        self.sendline('To: to@example.com')
+        self.sendline('Subject: this it test mail')
+        self.sendline('')
+        self.sendline('test')
+        self.sendline('.')
+
+    def do_dele(self):
+        self.sendline(b'+OK message deleted')
+
+    def do_apop(self):
+        self.sendline(b'+OK apop login successful')
+
+    def do_user(self):
+        self.sendline(b'+OK valid user name')
+
+    def do_pass(self):
+        self.sendline(b'+OK login successful')
+
+    def do_quit(self):
+        self.sendline(b'+OK')
+        raise EOFError
+
+    def talkback(self, line):
+        if not line:
+            raise EOFError
+        cmd = line.strip().split(b' ')[0].decode().lower()
+        getattr(self, 'do_' + cmd)()
+
+    # Running server
+    def run(self):
+        self.accept()
+        self.greeting()
+        while True:
+            line = self.recvline()
+            try:
+                self.talkback(line)
+            except EOFError:
+                break
+        self.exit()
