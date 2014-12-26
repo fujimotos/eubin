e62c5cfd6ae61877deb022a4a0c2297e8a2f942a
Fujimoto Seiji <fujimoto@ceptord.net>
2014-12-26 18:38:56 +0900

    Simplify the implementation of 'POP3Server' class

--- test/mockserver.py
+++ test/mockserver.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python3
 
 import socket
-import os
-import time
 
-class MockServer:
-    """A base class for server stub"""
+class POP3Server:
     def __init__(self, host='localhost', port=0):
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.bind((host, port))
         self.server_socket.listen(1)
         self.client_socket = None
 
-        # The MockServer class is intended to be used in a 'log-based' testing
+        self.respdict = None
+
+        # The POP3Server class is intended to be used in a 'log-based' testing
         # for client scripts. It buffers all the input passed by the client, 
         # so, after executing some test pattern, you can inspect the log and
         # check if the desired conditions were fulfilled.
-        self._recvlog = []
+        self.recvlog = []
 
     def accept(self):
         socket, address = self.server_socket.accept()
@@ -27,16 +26,17 @@ class MockServer:
             self.client_socket.close()
         self.server_socket.close()
 
+    def set_respdict(self, respdict):
+        self.respdict = respdict
+
     def get_conninfo(self):
         return self.server_socket.getsockname()
 
     def get_logiter(self):
-        return (line for line in self._recvlog)
+        return (line for line in self.recvlog)
 
-    # Low level API
-    def sendline(self, data=b'', eol=b'\r\n'):
-        line = data + eol
-        self.client_socket.send(line)
+    def greeting(self):
+        self.client_socket.send(self.respdict['greeting'])
 
     def recvline(self, eol=b'\r\n'):
         line = b''
@@ -47,62 +47,18 @@ class MockServer:
             line += bt
             if line[-2:] == eol:
                 break
-        self._recvlog.append(line)
+        self.recvlog.append(line)
         return line
 
-class POP3Server(MockServer):
-    """A POP3 server stub"""
-    def __init__(self):
-        super(POP3Server, self).__init__()
-
-        # fake data
-        self.msg_id = b''
-        self.maildrop = []
-        self.mailcount = 0
-        self.dropsize = 0
-
-    def set_fakedata(self, msg_id, maildrop, mailcount, dropsize):
-        self.msg_id = msg_id
-        self.maildrop = maildrop
-        self.mailcount = mailcount
-        self.dropsize = dropsize
-
-    def greeting(self):
-        self.sendline(b'+OK Greetings, human! ' + self.msg_id)
-
-    # Stub methods
-    def do_stat(self):
-        resp = '+OK {} {}'.format(self.mailcount, self.dropsize).encode()
-        self.sendline(resp)
-
-    def do_retr(self, idx):
-        self.sendline(b'+OK')
-        for line in self.maildrop[idx]:
-            self.sendline(line)
-        self.sendline(b'.')
-
-    def do_dele(self, idx):
-        self.sendline(b'+OK message deleted')
-
-    def do_apop(self, user, md5):
-        self.sendline(b'+OK apop login successful')
-
-    def do_user(self, user):
-        self.sendline(b'+OK valid user name')
-
-    def do_pass(self, password):
-        self.sendline(b'+OK login successful')
-
-    def do_quit(self):
-        self.sendline(b'+OK')
-        raise EOFError
-
     def talkback(self, line):
-        if not line:
-            raise EOFError
-        tokens = line.decode().rstrip().split(' ')
-        cmd, args = tokens[0].lower(), tokens[1:]
-        getattr(self, 'do_' + cmd)(*args)
+        exit = False
+        line = line.decode().rstrip()
+        cmd = line.split(' ')[0].lower()
+        self.client_socket.send(self.respdict[cmd])
+
+        if cmd == 'quit':
+            exit = True
+        return exit
 
     # Running server
     def run(self):
@@ -110,8 +66,6 @@ class POP3Server(MockServer):
         self.greeting()
         while True:
             line = self.recvline()
-            try:
-                self.talkback(line)
-            except EOFError:
+            if not line or self.talkback(line):
                 break
         self.exit()
