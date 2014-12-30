1b6d1eb96ff26b21ac39ff689a4b33fe718bdbfb
Fujimoto Seiji <fujimoto@ceptord.net>
2014-12-31 01:08:55 +0900

    Clean up bdist.py a bit.

--- bin/bdist.py
+++ bin/bdist.py
@@ -4,35 +4,32 @@ from zipfile import ZipFile
 import os
 import glob
 
-tmpfile = 'bin/eubin.zip'
-bdist = 'bin/eubin'
+ZIPFILE = 'bin/eubin.zip'
+BINFILE = 'bin/eubin'
+MAIN_SCRIPT = b'''\
+from eubin import main
+if __name__ == "__main__":
+    main.main()
+'''
 
 # chdir
 filedir = os.path.dirname(__file__)
 os.chdir(os.path.join(filedir, '..'))
 
-# Bundling eubin
-with ZipFile(tmpfile, mode='w') as zipfile:
+# Bundling modules
+with ZipFile(ZIPFILE, mode='w') as zipfile:
     for path in glob.glob('eubin/*.py'):
         zipfile.write(path)
 
     # Define entry point
-    script = b'''\
-from eubin import main
-if __name__ == "__main__":
-    main.main()
-'''
-    zipfile.writestr('__main__.py', script)
+    zipfile.writestr('__main__.py', MAIN_SCRIPT)
 
-    zipfile.close()
+# Generate executable
+with open(BINFILE, 'wb') as fw:
+    fw.write(b'#!/usr/bin/env python3\n')
+    fw.write(open(ZIPFILE, 'rb').read())
 
-# Create executable
-with open(bdist, 'wb') as fw:
-    fw.write(b'#!/usr/bin/env python3\n\n')
-    fw.write(open(tmpfile, 'rb').read())
+os.chmod(BINFILE, 0o755)
+os.remove(ZIPFILE)
 
-os.remove(tmpfile)
-
-os.chmod(bdist, 0o755)
-
-print('Executable:', bdist)
+print('Executable:', BINFILE)
