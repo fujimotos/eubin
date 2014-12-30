325c13ee8825a23f9746c4b4cdf73bca22870407
Fujimoto Seiji <fujimoto@ceptord.net>
2014-12-30 22:26:16 +0900

    Rewrite main.py

--- eubin/main.py
+++ eubin/main.py
@@ -1,71 +1,77 @@
-CONF_DIR = os.path.expanduser('~/.eubin')
-CONF_SUFFIX = '.conf'
+import getopt
+import sys
+import os
+import logging
+_log = logging.getLogger(__name__)
+from eubin import pop3
 
-LOG_DIR = os.path.join(CONF_DIR, 'log')
-LOG_FORMAT = '%(asctime)s\t%(message)s'
-LOG_ROTATE = (30, 'd')  # 30 days
+def get_config():
+    import configparser
+    import glob
+
+    os.chdir(os.path.expanduser('~/.eubin'))
+
+    for filename in glob.iglob('*.conf'):
+        config = configparser.ConfigParser()
+        config.read(filename)
+        yield (filename, config)
+
+def get_password(token, passtype):
+    password = None
+
+    if passtype == 'plain':
+        password = token
+    elif passtype == 'shell':
+        import shlex
+        from subprocess import check_output
+        cmd = shlex.split(token)
+        password = check_output(cmd).decode().strip()
+    else:
+        raise ValueError('unknown password type: {}'.format(passtype))
+
+    return password
 
 def main():
-    import getopt, sys
-
-    # Parameter processing
-    debug_level = logging.INFO
+    debug_level = logging.WARNING
 
     opts, args = getopt.getopt(sys.argv[1:], 'v')
     for key, val in opts:
         if key == '-v':
             debug_level -= 10
 
-    # Configure logging
-    _log.setLevel(level=debug_level)
+    logging.basicConfig(level=debug_level)
 
     # Main
-    for filename, config in get_configs(CONF_DIR, CONF_SUFFIX):
+    for filename, config in get_config():
         server, account, retrieval, security = \
             (config[key] for key in ('server', 'account', 'retrieval', 'security'))
-
-        # Setting logging
-        logfile = filename.replace(CONF_SUFFIX, '.log')
-        handler = logging.handlers.TimedRotatingFileHandler(
-            filename = os.path.join(LOG_DIR, logfile),
-            interval = LOG_ROTATE[0],
-            when = LOG_ROTATE[1]
-        )
-        handler.setFormatter(logging.Formatter(LOG_FORMAT))
-        _log.addHandler(handler)
 
         # Initiate the connection
         host, port = server['host'], server['port']
         overssl = security.getboolean('overssl')
 
-        _log.info('Connect to %s:%s [SSL=%s].', host, port, overssl)
-
         if overssl:
-            eubin = EubinSSL(host, port)
+            client = pop3.ClientSSL(host, port)
         else:
-            eubin = Eubin(host, port)
+            client = pop3.Client(host, port)
 
         # Authorization
         user = account['user']
         password = get_password(account['pass'], account['passtype'])
         apop = security.getboolean('apop')
 
-        _log.info("Login as '%s' [APOP=%s]", user, apop)
-        eubin.login(user, password, apop=apop)
+        client.login(user, password, apop=apop)
 
         # Do some transaction
         dest = os.path.expanduser(retrieval['dest'])
         leavecopy = retrieval.getboolean('leavecopy')
 
-        _log.info('Start fetching mails to %s [leavecopy=%s]', dest, leavecopy)
-        stat = eubin.fetchmail(dest, leavecopy=leavecopy)
+        stat = client.fetchmail(dest, leavecopy=leavecopy)
+
+        _log.info('[%s] %s mails retrieved (%s bytes)', filename, *stat)
 
         # Enter the update state.
-        _log.info("Delivered: %s mails (%s bytes)", *stat)
-        eubin.quit()
-
-        # Detach logging handler
-        _log.removeHandler(handler)
+        client.quit()
 
 if __name__ == '__main__':
     main()
