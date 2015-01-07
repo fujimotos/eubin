ba3a0af4b90ba4441dc9fa0bfb16f2dd33f975d1
Fujimoto Seiji <fujimoto@ceptord.net>
2015-01-08 01:07:59 +0900

    Add test cases for fetchmail_copy

--- test/test_pop3.py
+++ test/test_pop3.py
@@ -18,7 +18,8 @@ class TestPOP3(unittest.TestCase):
             'stat': b'+OK 2 320\r\n',
             'dele': b'+OK Mark the mail as deleted.\r\n',
             'retr': b'+OK\r\n<mail-text>\r\n.\r\n',
-            'quit': b'+OK Good bye!'
+            'quit': b'+OK Good bye!',
+            'top': b'+OK\r\n<header>\r\n.\r\n',
         })
         self.host, self.port = self.server.get_conninfo()
         Thread(target=self.server.run).start()
@@ -28,8 +29,12 @@ class TestPOP3(unittest.TestCase):
         for dirname in ('new', 'cur', 'tmp'):
             os.mkdir(os.path.join(self.mailbox, dirname))
 
+        # Set up a hashlog
+        self.hashlog = tempfile.NamedTemporaryFile()
+
     def tearDown(self):
         shutil.rmtree(self.mailbox)
+        self.hashlog.close()
 
     def test_login(self):
         client = pop3.Client(self.host, self.port)
@@ -52,21 +57,6 @@ class TestPOP3(unittest.TestCase):
         self.assertEqual(next(recvlog), b'APOP user 88670a99aa1930515aae5569677fac19\r\n')
         self.assertEqual(next(recvlog), b'QUIT\r\n')
 
-#    def test_fetchmail(self):
-#        client = pop3.Client(self.host, self.port)
-#        client.login('user', 'password')
-#        client.fetchmail(self.mailbox)
-#        client.quit()
-#
-#        recvlog = self.server.get_logiter()
-#
-#        self.assertEqual(next(recvlog), b'USER user\r\n')
-#        self.assertEqual(next(recvlog), b'PASS password\r\n')
-#        self.assertEqual(next(recvlog), b'STAT\r\n')
-#        self.assertEqual(next(recvlog), b'RETR 1\r\n')
-#        self.assertEqual(next(recvlog), b'RETR 2\r\n')
-#        self.assertEqual(next(recvlog), b'QUIT\r\n')
-
     def test_fetchmail(self):
         client = pop3.Client(self.host, self.port)
         client.login('user', 'password')
@@ -82,6 +72,42 @@ class TestPOP3(unittest.TestCase):
         self.assertEqual(next(recvlog), b'DELE 1\r\n')
         self.assertEqual(next(recvlog), b'RETR 2\r\n')
         self.assertEqual(next(recvlog), b'DELE 2\r\n')
+        self.assertEqual(next(recvlog), b'QUIT\r\n')
+
+    def test_fetchmail_copy(self):
+        client = pop3.Client(self.host, self.port)
+        client.login('user', 'password')
+        client.fetchmail_copy(self.mailbox, logpath=self.hashlog.name)
+        client.quit()
+
+        recvlog = self.server.get_logiter()
+
+        self.assertEqual(next(recvlog), b'USER user\r\n')
+        self.assertEqual(next(recvlog), b'PASS password\r\n')
+        self.assertEqual(next(recvlog), b'STAT\r\n')
+        self.assertEqual(next(recvlog), b'TOP 1 0\r\n')
+        self.assertEqual(next(recvlog), b'RETR 1\r\n')
+        self.assertEqual(next(recvlog), b'TOP 2 0\r\n')
+        self.assertEqual(next(recvlog), b'RETR 2\r\n')
+        self.assertEqual(next(recvlog), b'QUIT\r\n')
+
+    def test_fetchmail_copy_retlieved(self):
+        with open(self.hashlog.name, 'w') as fp:
+            # md5sum '<header>'
+            fp.write('cea964ac6a233b51fe5a28f7f4a40895\n')
+
+        client = pop3.Client(self.host, self.port)
+        client.login('user', 'password')
+        client.fetchmail_copy(self.mailbox, logpath=self.hashlog.name)
+        client.quit()
+
+        recvlog = self.server.get_logiter()
+
+        self.assertEqual(next(recvlog), b'USER user\r\n')
+        self.assertEqual(next(recvlog), b'PASS password\r\n')
+        self.assertEqual(next(recvlog), b'STAT\r\n')
+        self.assertEqual(next(recvlog), b'TOP 1 0\r\n')
+        self.assertEqual(next(recvlog), b'TOP 2 0\r\n')
         self.assertEqual(next(recvlog), b'QUIT\r\n')
 
     def test_fetchmail_contents(self):
