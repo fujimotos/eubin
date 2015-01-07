2ce2789d20f85690465ad3630347130406b457b0
Fujimoto Seiji <fujimoto@ceptord.net>
2015-01-08 00:21:37 +0900

    Add -p <pattern> option to the test runner.

--- runtest.py
+++ runtest.py
@@ -9,19 +9,22 @@ import os
 TEST_DIR = os.path.join(os.path.dirname(__file__), 'test')
 
 def main():
-    opts, args = getopt.getopt(sys.argv[1:], 'v')
+    opts, args = getopt.getopt(sys.argv[1:], 'v:p:')
     verbosity = 1
+    pattern = 'test*.py'
 
     for key, val in opts:
         if key == '-v':
             verbosity += 1
+        elif key == '-p':
+            pattern = val
 
     # Disable the logging outputs while testing except for
     # ERROR or FATAL.
     logging.disable(logging.WARNING)
 
     # Correct all the test modules in the TEST_DIR.
-    suite = unittest.defaultTestLoader.discover(TEST_DIR)
+    suite = unittest.defaultTestLoader.discover(TEST_DIR, pattern=pattern)
 
     runner = unittest.TextTestRunner(verbosity=verbosity)
     runner.run(suite)
