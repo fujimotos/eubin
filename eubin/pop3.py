fc260970b2d6fd6a4a41cddd32f0edde8ee66664
Fujimoto Seiji <fujimoto@ceptord.net>
2015-01-12 18:23:33 +0900

    Allow to see what options are set in the SSL connection.

--- eubin/pop3.py
+++ eubin/pop3.py
@@ -79,7 +79,8 @@ class ClientSSL(Client):
 
         self._state['ssl'] = {
             'version': ssl.OPENSSL_VERSION,
-            'cipher': self.pop3.sock.cipher()
+            'cipher': self.pop3.sock.cipher(),
+            'option': self._decode_ssl_options(context.options)
         }
 
     @staticmethod
@@ -109,6 +110,50 @@ class ClientSSL(Client):
         context.set_default_verify_paths()
 
         return context
+
+    @staticmethod
+    def _decode_ssl_options(options):
+        # This is the full list of ssl options defined in include/ssl.h
+        # at openssl-1.0.1k.
+        openssl_options = {
+            0x00000001: 'SSL_OP_MICROSOFT_SESS_ID_BUG',
+            0x00000002: 'SSL_OP_NETSCAPE_CHALLENGE_BUG',
+            0x00000004: 'SSL_OP_LEGACY_SERVER_CONNECT',
+            0x00000008: 'SSL_OP_NETSCAPE_REUSE_CIPHER_CHANGE_BUG',
+            0x00000010: 'SSL_OP_TLSEXT_PADDING',
+            0x00000020: 'SSL_OP_MICROSOFT_BIG_SSLV3_BUFFER',
+            0x00000040: 'SSL_OP_SAFARI_ECDHE_ECDSA_BUG',
+            0x00000080: 'SSL_OP_SSLEAY_080_CLIENT_DH_BUG',
+            0x00000100: 'SSL_OP_TLS_D5_BUG',
+            0x00000200: 'SSL_OP_TLS_BLOCK_PADDING_BUG',
+            0x00000800: 'SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS',
+            0x80000BFF: 'SSL_OP_ALL',
+            0x00001000: 'SSL_OP_NO_QUERY_MTU',
+            0x00002000: 'SSL_OP_COOKIE_EXCHANGE',
+            0x00004000: 'SSL_OP_NO_TICKET',
+            0x00008000: 'SSL_OP_CISCO_ANYCONNECT',
+            0x00010000: 'SSL_OP_NO_SESSION_RESUMPTION_ON_RENEGOTIATION',
+            0x00020000: 'SSL_OP_NO_COMPRESSION',
+            0x00040000: 'SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION',
+            0x00080000: 'SSL_OP_SINGLE_ECDH_USE',
+            0x00100000: 'SSL_OP_SINGLE_DH_USE',
+            0x00400000: 'SSL_OP_CIPHER_SERVER_PREFERENCE',
+            0x00800000: 'SSL_OP_TLS_ROLLBACK_BUG',
+            0x01000000: 'SSL_OP_NO_SSLv2',
+            0x02000000: 'SSL_OP_NO_SSLv3',
+            0x04000000: 'SSL_OP_NO_TLSv1',
+            0x08000000: 'SSL_OP_NO_TLSv1_2',
+            0x10000000: 'SSL_OP_NO_TLSv1_1',
+            0x20000000: 'SSL_OP_NETSCAPE_CA_DN_BUG',
+            0x40000000: 'SSL_OP_NETSCAPE_DEMO_CIPHER_CHANGE_BUG',
+            0x80000000: 'SSL_OP_CRYPTOPRO_TLSEXT_BUG'
+        }
+
+        res = []
+        for key in sorted(openssl_options.keys()):
+            if key & options:
+                res.append(openssl_options[key])
+        return res
 
 # An interface function for Client/ClientSSL.
 # Use this function as follows:
