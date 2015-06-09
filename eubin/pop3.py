4b2bc12a4d6645619ae6dffda1fec637cac1e27e
Fujimoto Seiji <fujimoto@ceptord.net>
2015-06-09 22:38:09 +0900

    Use the default context generator provided by the ssl module.

--- eubin/pop3.py
+++ eubin/pop3.py
@@ -51,36 +51,8 @@ class Client:
 
 class ClientSSL(Client):
     def __init__(self, host, port):
-        context = self.get_ssl_context()
+        context = ssl.create_default_context()
         self.pop3 = poplib.POP3_SSL(host, port, context=context)
-
-    @staticmethod
-    def get_ssl_context():
-        # These settings below are based on ssl.get_default_context()
-        # method (intoroduced in Python 3.4) with some modifications.
-        # cf. https://docs.python.org/3.4/library/ssl.html#ssl.create
-        #     _default_context
-        context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
-
-        # SSLv2/SSLv3 is known to be insecure. 
-        # TODO: Wait untill Python3.4 is widely adapted enough, then
-        #       disable TLSv1.0 as well.
-        context.options |= ssl.OP_NO_SSLv2
-        context.options |= ssl.OP_NO_SSLv3
-
-        # Disable TLS compression
-        context.options |= ssl.OP_NO_COMPRESSION
-
-        # Disable some insecure ciphres
-        context.set_ciphers('HIGH:!NULL:!eNULL:!aNULL:!RC4:!DSS:!MD5')
-
-        # Always requires certifications.
-        context.verify_mode = ssl.CERT_REQUIRED
-
-        # Load default certificates.
-        context.set_default_verify_paths()
-
-        return context
 
 
 # An interface function for Client/ClientSSL.
